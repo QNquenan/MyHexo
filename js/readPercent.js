f396e9b1739656f812900427d62dcc75cc106c65
qnquenan <qn2987271942@outlook.com>
2024-03-26 20:25:23 +0800

    Site updated: 2024-03-26 20:25:19

--- js/readPercent.js
+++ js/readPercent.js
@@ -1,17 +1,20 @@
 window.onscroll = percent;// 执行函数
 // 页面百分比
 function percent() {
-    let a = document.documentElement.scrollTop || window.pageYOffset, // 卷去高度
+    var a = document.documentElement.scrollTop || window.pageYOffset, // 卷去高度
         b = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight, document.body.offsetHeight, document.documentElement.offsetHeight, document.body.clientHeight, document.documentElement.clientHeight) - document.documentElement.clientHeight, // 整个网页高度
         result = Math.round(a / b * 100), // 计算百分比
-        up = document.querySelector("#go-up") // 获取按钮
+        up = document.querySelector("#go-up"),
+        upi = document.querySelector("#go-up i"),// 获取按钮
+        upNub = document.querySelector(".scroll-percent")
 
     if (result <= 95) {
-        up.childNodes[1].style.display = 'none'
-        up.childNodes[0].style.display = 'block'
-        up.childNodes[0].innerHTML = result;
+        upi.style.display = 'none'
+        upNub.style.display = 'block'
+        upNub.innerHTML = result;
     } else {
-        up.childNodes[0].style.display = 'none'
-        up.childNodes[1].style.display = 'block'
+        upNub.style.display = 'none'
+        upi.style.display = 'block'
     }
+
 }
