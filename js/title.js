2b529be526241f5d1041b87e1478addc68484a88
qnquenan <qn2987271942@outlook.com>
2024-04-22 15:59:20 +0800

    Site updated: 2024-04-22 15:59:18

--- js/title.js
+++ js/title.js
@@ -1,17 +1,1 @@
-//动态标题
-var OriginTitile = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-    if (document.hidden) {
-        //离开当前页面时标签显示内容
-        document.title = '(o゜▽゜)o☆人呢人呢';
-        clearTimeout(titleTime);
-    } else {
-        //返回当前页面时标签显示内容
-        document.title = 'ヾ(≧▽≦*)o捉~';
-        //两秒后变回正常标题
-        titleTime = setTimeout(function () {
-            document.title = OriginTitile;
-        }, 2000);
-    }
-});
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="(o゜▽゜)o☆人呢人呢",clearTimeout(titleTime)):(document.title="ヾ(≧▽≦*)o捉~",titleTime=setTimeout((function(){document.title=OriginTitile}),2e3))}));
